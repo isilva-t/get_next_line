a6f69b0bac30e136fa342971f3f1282e681b76a3
Ivan Rafael Silva Teixeira <isilva-t@c1r11s8.42porto.com>
2024-05-16 18:44:59 +0100

    make a tester

--- project/get_next_line.c
+++ project/get_next_line.c
@@ -12,64 +12,84 @@
 
 #include "get_next_line.h"
 
-static char	*ft_read_line(int fd, char *buf, char *backup_static)
+static char	*ft_search_line(int fd, char *buf, char *backup_static)
 {
 	int		read_check;
-	char	*temp;
+	char	*temp_to_free;
 
+	//read_check comeca por ter valor 1, para que o primeiro while
+	//{pelo menos} funcione, uma vez que se for menos que zero
 	read_check = 1;
+	//enquanto eu tenho read_check
+printf("1_FT_SEARCH_LINE: \n");
 	while (read_check)
 	{
+printf("read_check before \"read\": [ %d ]\n", read_check);
+		//read_check recebe o nr de caracteres lidos
 		read_check = read(fd, buf, BUFFER_SIZE);
+
+		//se o read der erro, retorna NULL pointer, nao ha linha.
 		if (read_check == -1)
-			return (0);
+			return (NULL);
+		// se o meu read_check for zero, nao ha mais nada a ler
+		//para o while
 		else if (read_check == 0)
+		{
+printf("read_check AFTER \"read\" == [ 0 ]!!!!!!!!\n");
 			break ;
-		/*********************************************************/	
-		buf[read_ok] = '\0';
-		/*********************************************************/	
+		}
+		buf[read_check] = '\0';
+	
 		if (!backup_static)
 			backup_static = ft_strdup("");
-		/*********************************************************/		
-		temp = backup_static;
-		backup_static = (ft_strjoin(temp, buf));
+	
+		temp_to_free = backup_static;
+		backup_static = (ft_strjoin(temp_to_free, buf));
 		if (!backup_static)
 			return (NULL);
-		/*********************************************************/		
-		free (temp);
-		temp = NULL;
-		/*********************************************************/	
+		
+		free (temp_to_free);
+		temp_to_free = NULL;
+
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
-	/*********************************************************/
+
+if (read_check > 0)
+	printf("READ_CHECK:[ %d ]\n",  read_check);	
+printf("LAST BUFFER:[ %zd ]\n",  ft_strlen(buf));
+printf("********* STATIC_VARIABLE_CONTENT_IN_FT_SEARCH_LINE ********* \
+\n%s\n********************************************************\n", backup_static);
+
 	return (backup_static);
 }
 
 static char	*ft_rest(char *line)
 {
 	int		i;
-	char	*temp;
+	char	*rest;
 
 	i = 0;
 	while (line[i] != '\0' && line[i] != '\n')
 		i++;
 	if (line[i] == '\0')
 		return (NULL);
-	/*********************************************************/
-	temp = ft_substr(line, i + 1, ft_strlen(line) - i);
-	if (!temp)
+
+	rest = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (!rest)
 		return (NULL);
-	if (temp[0] == '\0')
+	//- preciso prevencao para se o array criado esta' vazio
+	// se sim, dar free, array a NULL, e return NULL
+	if (!rest[0])
 	{
-		free (temp);
-		temp = NULL;
+		free (rest);
+		rest = NULL;
 		return (NULL);
 	}
-	/*********************************************************/
+	// preencher ultimo caractere do resto com \0 terminator
 	line[i + 1] = '\0';
-	/*********************************************************/
-	return (temp);
+
+	return (rest);
 }
 
 char	*get_next_line(int fd)
@@ -78,20 +98,29 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	*backup_static;
 
+	//se o fd for menor que zero(erro) 
+	//ou 
+	//o buffer_size menor ou igual a zero
+	//retorna NULL
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	/*********************************************************/	
+
+	//aloca memoria para o buffer, com tamanho do buffer size mais \0 terminator
 	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buf)
 		return (NULL);
-	/*********************************************************/	
-	line = ft_read_line(fd, buf, backup_static);
+
+	//linha a retornar recebe read_line, com parametros: valor fd, buffer, e backup_static
+	line = ft_search_line(fd, buf, backup_static);
 	free(buf);
-	/*********************************************************/
 	if (!line)
 		return (NULL);
-	/*********************************************************/	
-	backup = ft_rest(line);
-	/*********************************************************/
+
+	//depois de ter a linha a retornar, vamos buscar os restantes caracteres ao buffer
+	//(se existirem) no nosso ponteiro static
+	backup_static = ft_rest(line);
+printf("3_STATIC__AFTER__FT_REST:\n%s\n[END_STATIC]\n", backup_static);
+
+	//retorna a linha lida (ou ficheiro se chegar a EOF)
 	return (line);
 }
--- project/get_next_line.h
+++ project/get_next_line.h
@@ -15,11 +15,11 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
 
-#ifndef BUFFER_SIZE
-#	define BUFFER_SIZE 10
+#include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 15
 # endif
 
 char	*get_next_line(int fd);
